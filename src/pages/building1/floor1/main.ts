3aed153f52fed7557c1efdee27a57bbee76977fb
any <130113998+a-neko@users.noreply.github.com>
2024-02-20 00:47:51 +0900

    Any/refactoring (#5)
    * フロントの修正およびlintの修正
    
    * refactoringの実施
    
    * script type=importmapの削除
    
    * 置換ミスの修正
    
    * main関数の分割
    
    * ディレクトリ構造の見直し
    
    * スマホでの環境整備とレスポンシブデザインの実装
    
    ---------
    
    Co-authored-by: darallium <46854880+darallium@users.noreply.github.com>

---
+++ src/pages/building1/floor1/main.ts
@@ -0,0 +1,45 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import {Road} from "./ref/ground/road.ts";
+
+
+function main() {
+  const canvas = document.querySelector("#c") as HTMLCanvasElement;
+  const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+  const fov = 45;
+  const aspect = 2; // the canvas default
+  const near = 0.1;
+  const far = 100;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 10, 20);
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color("black");
+
+  {
+    const skyColor = 0xb1e1ff; // light blue
+    const groundColor = 0xb97a20; // brownish orange
+    const intensity = 2;
+    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+    scene.add(light);
+  }
+
+  function render() {
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+
+}
+
+main();
+Road();
+
+
