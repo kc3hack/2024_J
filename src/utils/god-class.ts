133240da9a973552c27a435179d388c5b8129ae2
darallium <annihilation0504@gmail.com>
2024-02-20 14:22:36 +0900

    あげるものまちがった

--- src/utils/god-class.ts
+++ src/utils/god-class.ts
@@ -1,28 +1,16 @@
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import * as THREE from "three";
-import { frameArea } from "../pages/building1/floor1/ref/frameArea.ts";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import {RGBELoader} from "three/examples/jsm/loaders/RGBELoader.js";
+import {DataTexture} from "three";
 
-export function drawSkybox(
-  scene: THREE.Scene,
-  camera: THREE.PerspectiveCamera,
-  controls: OrbitControls,
-  renderer: THREE.WebGLRenderer,
-) {
-  renderer.toneMapping = THREE.ACESFilmicToneMapping;
-  renderer.toneMappingExposure = 0.8;
-
-  const gltfLoader = new GLTFLoader();
-  const url = "src/3d/model/plane.gltf";
-  gltfLoader.load(url, (gltf) => {
-    const root = gltf.scene;
-    scene.add(root);
-    const box = new THREE.Box3().setFromObject(root);
-    const boxSize = box.getSize(new THREE.Vector3()).length();
-    const boxCenter = box.getCenter(new THREE.Vector3());
-    frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
-    controls.maxDistance = boxSize * 10;
-    controls.target.copy(boxCenter);
-    controls.update();
-  });
+export function drawSkybox(scene: THREE.Scene){
+    const loader = new RGBELoader();
+    const url = "skybox/kloppenheim_02_puresky_4k.hdr";
+    const textures = loader.load(url, (texture: DataTexture) => {
+        texture.mapping = THREE.EquirectangularReflectionMapping;
+        texture.minFilter = THREE.LinearFilter;
+        texture.magFilter = THREE.LinearFilter;
+        texture.needsUpdate = false;
+    });
+    scene.background = textures;
+    scene.environment = textures;
 }
