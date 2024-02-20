bbbfb56131cefa588c7cbceef8543c0ad4abe319
darallium <annihilation0504@gmail.com>
2024-02-20 19:21:23 +0900

    ふにゃ

--- src/utils/god-class.ts
+++ src/utils/god-class.ts
@@ -3,8 +3,9 @@ import * as THREE from "three";
 export class GPSWrap {
   gps_origin: THREE.Vector2;
   map_origin: THREE.Vector2;
-  scale: number; // gps / map
+  scale1: number; // gps / map
   scale2: number; // map / gps
+  gps_now: THREE.Vector2; // (lat, lon)
   constructor(
     gps_origin: THREE.Vector2,
     gps_point: THREE.Vector2,
@@ -13,7 +14,7 @@ export class GPSWrap {
   ) {
     this.gps_origin = gps_origin;
     this.map_origin = map_origin;
-    this.scale = GPSWrap.calculateScale(
+    this.scale1 = GPSWrap.calculateScale(
       gps_origin,
       gps_point,
       map_origin,
@@ -25,6 +26,7 @@ export class GPSWrap {
       gps_origin,
       gps_point,
     );
+    this.gps_now = this.getGPSPos();
   }
   private static calculateScale(
     origin_A: THREE.Vector2,
@@ -40,7 +42,7 @@ export class GPSWrap {
     return gps
       .clone()
       .sub(this.gps_origin)
-      .multiplyScalar(this.scale)
+      .multiplyScalar(this.scale1)
       .add(this.map_origin);
   }
   public mapToGps(map: THREE.Vector2): THREE.Vector2 {
@@ -49,5 +51,22 @@ export class GPSWrap {
       .sub(this.map_origin)
       .multiplyScalar(this.scale2)
       .add(this.gps_origin);
+  }
+  public getGPSPos(): THREE.Vector2 {
+    let pos: THREE.Vector2;
+    pos = new THREE.Vector2(Infinity, Infinity);
+    if (!navigator.geolocation) throw new Error("GPS is not available");
+    const Ok = (position: GeolocationPosition) => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      pos = this.gps_now = new THREE.Vector2(lat, lon);
+      console.log(`${lat}, ${lon}`);
+    };
+    const Ng = () => {
+      console.log("kokodoko...");
+      throw new Error("GPS is not available");
+    };
+    navigator.geolocation.getCurrentPosition(Ok, Ng);
+    return pos;
   }
 }
