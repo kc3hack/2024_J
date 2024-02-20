45de4c3326951b0de162ef26720251dcfd2e693d
darallium <46854880+darallium@users.noreply.github.com>
2024-02-20 18:48:24 +0900

    Darallium/skybox (#6)
    * チーム名差し替え
    
    * added skybox images
    
    * ignorance
    
    * exrだとでかすぎるので却下
    
    * HDRIに変更
    
    * skyboxの描画
    
    * あげるものまちがった
    
    * あげるものまちがった
    
    * もう画像使ってないので
    
    * せっかくなのでignoreもいいや
    
    * Delete public/KC3.bin
    
    * Delete public/KC3.gltf
    
    * Delete src/3d/model directory
    
    * 微調整 (#11)
    
    ---------
    
    Co-authored-by: any <130113998+a-neko@users.noreply.github.com>

---
+++ src/utils/god-class.ts
@@ -0,0 +1,15 @@
+import * as THREE from "three";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
+import { DataTexture } from "three";
+export function drawSkybox(scene: THREE.Scene) {
+  const loader = new RGBELoader();
+  const url = "/skybox/kloppenheim_02_puresky_4k.hdr";
+  const textures = loader.load(url, (texture: DataTexture) => {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    texture.needsUpdate = false;
+  });
+  scene.background = textures;
+  scene.environment = textures;
+}
