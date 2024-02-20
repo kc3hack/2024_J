a538c0c5a1ba9214d3cd07682e4c30ca82ea4818
any <130113998+a-neko@users.noreply.github.com>
2024-02-20 18:45:57 +0900

    微調整 (#11)

--- src/utils/god-class.ts
+++ src/utils/god-class.ts
@@ -1,10 +1,9 @@
 import * as THREE from "three";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
 import { DataTexture } from "three";
-
 export function drawSkybox(scene: THREE.Scene) {
   const loader = new RGBELoader();
-  const url = "skybox/kloppenheim_02_puresky_4k.hdr";
+  const url = "/skybox/kloppenheim_02_puresky_4k.hdr";
   const textures = loader.load(url, (texture: DataTexture) => {
     texture.mapping = THREE.EquirectangularReflectionMapping;
     texture.minFilter = THREE.LinearFilter;
