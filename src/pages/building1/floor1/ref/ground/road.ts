b1286f9f4110b7aa7e53607be047d99b8b56b8bb
any <130113998+a-neko@users.noreply.github.com>
2024-02-25 11:19:43 +0900

    Any/sphere (#26)
    * フロントの修正およびlintの修正
    
    * commit miss
    
    * git ignore
    
    * a

--- src/pages/building1/floor1/ref/ground/road.ts
+++ src/pages/building1/floor1/ref/ground/road.ts
@@ -7,12 +7,15 @@ import { objectSetting } from "../objectSetting.ts";
 import { drawSkybox } from "../../../../../utils/Skybox.ts";
 import { Object3D, Texture } from "three";
 import { Swipe } from "../../../../../utils/swipe.ts";
+import { getGraph } from "../../../../../utils/gps.ts";
+import { getMapPair } from "../../../../../utils/gps.ts";
+import { getCorrespond } from "../../../../../utils/gps.ts";
 
 interface Material {
   map?: Texture;
 }
 
-export function Road(
+export async function Road(
   gltfURL: string,
   prevURL: string | null,
   nextURL: string | null,
@@ -56,12 +59,21 @@ export function Road(
       controls.update();
     });
   }
+  const t = await getMapPair(1);
+  const u = await getCorrespond(1);
+  const graph = await getGraph(t, u);
+  console.log(graph);
+  const geometry = new THREE.SphereGeometry(1, 32, 32);
+  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const sphere = new THREE.Mesh(geometry, material);
+  scene.add(sphere);
   function gltfRender() {
     if (resizeRendererToDisplaySize(gltfRenderer)) {
       const canvas = gltfRenderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
+    sphere.position.y = 1;
 
     gltfRenderer.render(scene, camera);
 
